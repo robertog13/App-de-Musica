6f6b7117794adf86bbe9b51d7baf22628d5d4fc3
Roberto <robertog.adm@gmail.com>
2023-01-04 17:54:31 -0300

    Adiciona as navegaoes de site no Header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import { AiFillStar } from 'react-icons/ai'
+import { BiLogIn } from 'react-icons/bi'
 import context from '../../context';
 import disco  from "../../images/disco-icon.png";
-import config from "../../images/engrenagem.png"
 import "./Header.css"
 
 function Header() {
@@ -12,7 +15,9 @@ function Header() {
       <h1>TryTunes</h1>
       <div className='PerfilInfo'>
         <p>{ username }</p>
-        <img className='Config' src={ config } alt="Configurações" />
+        <Link to="/main"><FaHome className='Navegation' size={30}/></Link>
+        <Link to="/favorite"><AiFillStar className='Navegation' size={ 32 }/></Link>
+        <Link to="/"><BiLogIn className='Navegation' size={30} /></Link>
       </div>
     </div>
   );
